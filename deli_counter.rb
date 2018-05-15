e75dabea20d1481312d205e151a8d674a4a07831
Alex Wilson <fonzmp@gmail.com>
2018-05-15 01:20:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  current_length = array.size.to_i
+  if current_length == 0
+    puts "The line is currently empty."
+  else
+    index = 1
+    new_string = "The line is currently: "
+    array.each do |name|
+      new_string += "#{index}. #{name} "
+      index += 1
+    end
+    puts new_string
+  end
+end
