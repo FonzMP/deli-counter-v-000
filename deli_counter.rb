6f6700c774df2fa67fbbf69560d4c58e5cfd856b
Alex Wilson <fonzmp@gmail.com>
2018-05-15 01:38:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,6 @@ end
 
 def take_a_number(array, name)
   array.push(name)
-  index_adjust = array[name].index + 1
+  index_adjust = array[name].index
   puts "Welcome, #{name}. You are number #{index_adjust} in line."
 end
